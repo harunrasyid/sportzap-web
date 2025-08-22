bed2bf94c0149242b45329904ef7566663931d93
Harun <harunrasyid98@gmail.com>
2025-08-22 16:10:26 +0700

    - Feat: add useInformationBar custom hooks

--- src/modules/f1/pages/Meeting/Meeting.tsx
+++ src/modules/f1/pages/Meeting/Meeting.tsx
@@ -1,12 +1,15 @@
 import { VStack } from "@chakra-ui/react";
 import { styles } from "./Meeting.style";
 import { InformationBar } from "./components/InformationBar";
+import { useInformationBar } from "./hooks/useInformationBar";
 
 export const Meeting = () => {
+  const informationBar = useInformationBar();
+
   return (
     <VStack css={styles.page}>
       {/* Information Bar */}
-      <InformationBar />
+      <InformationBar {...informationBar} />
 
       {/* Main Content */}
 
